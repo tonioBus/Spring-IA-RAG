4a3266151acfb383207d019ce82c27b500081506
Anthony Bussani <bussania@gmail.com>
2025-05-10 09:45:15 +0200

    add management of recursive directories with pdf and html

--- src/main/java/com/aquila/ia/rag/DataLoaderService.java
+++ src/main/java/com/aquila/ia/rag/DataLoaderService.java
@@ -1,32 +1,69 @@
 package com.aquila.ia.rag;
 
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.jsoup.JsoupDocumentReader;
+import org.springframework.ai.reader.jsoup.config.JsoupDocumentReaderConfig;
 import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
 import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class DataLoaderService {
 
-    @Value("classpath:/compliance_chart.pdf")
-    private Resource pdfResource;
+    @Value("/RAG/")
+    private String rootPath;
 
     private final VectorStore vectorStore;
 
-    @PostConstruct
+    private final TokenTextSplitter tokenTextSplitter = new TokenTextSplitter();
+
     public void load() {
-        log.info("load()");
-        PagePdfDocumentReader pdfReader = new PagePdfDocumentReader(this.pdfResource,
+        load(new File(rootPath));
+    }
+
+    private void load(File file) {
+        log.info("load({})", file);
+        Arrays.stream(Objects.requireNonNull(file.listFiles())).forEach(
+                subFile -> {
+                    if (subFile.canRead()) {
+                        if (subFile.isDirectory()) load(subFile);
+                        else {
+                            final String filename = subFile.toString();
+                            List<Document> documents = switch (filename.substring(filename.lastIndexOf('.'))) {
+                                case ".html" -> loadHtml(filename);
+                                case ".pdf" -> loadPdf(filename);
+                                default -> {
+                                    log.warn("file not supported: {}", filename);
+                                    yield null;
+                                }
+                            };
+                            if (documents != null) {
+                                log.info("importing into vector DB file: {}", filename);
+                                this.vectorStore.accept(tokenTextSplitter.apply(documents));
+                                log.info("importation of file: {} done.", filename);
+                            }
+                        }
+                    } else log.error("Can not read file/dir:{}", subFile);
+                }
+        );
+    }
+
+    private List<Document> loadPdf(String filename) {
+        PagePdfDocumentReader pdfReader = new PagePdfDocumentReader("file://"+filename,
                 PdfDocumentReaderConfig.builder()
                         .withPageExtractedTextFormatter(ExtractedTextFormatter.builder()
                                 .withNumberOfBottomTextLinesToDelete(3)
@@ -35,8 +72,23 @@ public class DataLoaderService {
                         .withPagesPerDocument(1)
                         .build());
 
-        TokenTextSplitter tokenTextSplitter = new TokenTextSplitter();
-        this.vectorStore.accept(tokenTextSplitter.apply(pdfReader.get()));
+        return tokenTextSplitter.apply(pdfReader.get());
+    }
+
+    /**
+     * @return list of documents
+     */
+    private List<Document> loadHtml(String filename) {
+        JsoupDocumentReaderConfig config = JsoupDocumentReaderConfig.builder()
+                .selector("body") // Extract paragraphs within <article> tags
+                .charset("ISO-8859-1")  // Use ISO-8859-1 encoding
+                .includeLinkUrls(true) // Include link URLs in metadata
+                .metadataTags(List.of("author", "date")) // Extract author and date meta tags
+                .additionalMetadata("source", filename) // Add custom metadata
+                .build();
+
+        JsoupDocumentReader reader = new JsoupDocumentReader(filename, config);
+        return reader.get();
     }
 
     @Bean
--- src/main/java/com/aquila/ia/rag/controlers/LlmControler.java
+++ src/main/java/com/aquila/ia/rag/controlers/LlmControler.java
@@ -1,6 +1,7 @@
 package com.aquila.ia.rag.controlers;
 
 import com.aquila.ia.rag.ChatBotService;
+import com.aquila.ia.rag.DataLoaderService;
 import com.aquila.ia.rag.Message;
 import com.aquila.ia.rag.llm.Ollama;
 import lombok.RequiredArgsConstructor;
@@ -21,12 +22,19 @@ public class LlmControler {
 
     // final private Ollama ollama;
     final private ChatBotService chatBotService;
+    final private DataLoaderService dataLoaderService;
 
     @GetMapping(value = "/llm")
     public String getAnswer(@RequestParam final String prompt) {
         log.info("getAnswer({})", prompt);
         return chatBotService.createPrompt(prompt);
     }
+
+    @GetMapping(value = "/reload")
+    public void reload() {
+        dataLoaderService.load();
+    }
+
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")

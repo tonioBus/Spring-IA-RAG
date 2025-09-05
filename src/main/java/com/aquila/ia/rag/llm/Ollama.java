5aab07f5fe1d883d33cd47d4aa8ee9312c574b5c
Anthony Bussani <bussania@gmail.com>
2025-09-05 21:02:16 +0200

    make it work again (application.yaml seems not well done)

--- src/main/java/com/aquila/ia/rag/llm/Ollama.java
+++ src/main/java/com/aquila/ia/rag/llm/Ollama.java
@@ -1,19 +1,21 @@
 package com.aquila.ia.rag.llm;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Getter
 @Component
 public class Ollama {
 
-    @Value("${spring.ia.ollama.chat.options.model}")
+    // @Value("${spring.ia.ollama.chat.options.model}")
     private String modelName = "deepseek-r1:7b";
 
-    @Value("${spring.ia.ollama.chat.options.temperature}")
+    // @Value("${spring.ia.ollama.chat.options.temperature}")
     private double temperature = 0.7;
 
     private final OllamaChatModel chatModel;
@@ -21,6 +23,7 @@ public class Ollama {
     private final OllamaOptions ollamaOptions;
 
     public Ollama(OllamaChatModel chatModel) {
+        log.info("Build Ollama");
         this.chatModel = chatModel;
         this.ollamaOptions = OllamaOptions.builder()
                 .model(modelName)
